8632e05a02ea63498f84b03776c736eadea3f961
Pyunghyuk Yoo <yoophi@gmail.com>
2023-11-29 23:45:07 +0900

    terminal, cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
+use std::error::Error;
+use std::io;
+use crossterm::{ExecutableCommand, terminal};
+use crossterm::cursor::Hide;
+use crossterm::terminal::EnterAlternateScreen;
+
+fn main() -> Result<(), Box<dyn Error>> {
     println!("Hello, world!");
+
+    // terminal
+    let mut stdout = io::stdout();
+    terminal::enable_raw_mode()?;
+    stdout.execute(EnterAlternateScreen)?;
+    stdout.execute(Hide)?;
+
+    // cleanup
+    terminal::disable_raw_mode()?;
+
+    Ok(())
 }

cccd3704bc67c7858dcff644dec7e909a44092d1
Pyunghyuk Yoo <yoophi@gmail.com>
2023-11-30 00:06:51 +0900

    render

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use std::error::Error;
-use std::io;
+use std::{io, thread};
+use std::sync::mpsc;
 use std::time::Duration;
 use crossterm::{event, ExecutableCommand, terminal};
-use crossterm::cursor::Hide;
+use crossterm::cursor::{Hide, Show};
 use crossterm::event::{Event, KeyCode};
-use crossterm::terminal::EnterAlternateScreen;
+use crossterm::terminal::{EnterAlternateScreen, LeaveAlternateScreen};
+use invaders::{frame, render};
+use invaders::frame::new_frame;
 
 fn main() -> Result<(), Box<dyn Error>> {
     println!("Hello, world!");
@@ -15,8 +18,29 @@ fn main() -> Result<(), Box<dyn Error>> {
     stdout.execute(EnterAlternateScreen)?;
     stdout.execute(Hide)?;
 
+    // init screen
+    let (render_tx, render_rx) = mpsc::channel();
+    let render_handle = thread::spawn(move || {
+        let mut last_frame = frame::new_frame();
+        let mut stdout = io::stdout();
+
+        render::render(&mut stdout, &last_frame, &last_frame, true);
+        loop {
+            let curr_frame = match render_rx.recv() {
+                Ok(x) => x,
+                Err(_) => break,
+            };
+            render::render(&mut stdout, &last_frame, &curr_frame, false);
+            last_frame = curr_frame;
+        }
+    });
+
+
     // game loop
     'gameloop: loop {
+        let curr_frame = new_frame();
+
+        // input
         while event::poll(Duration::default())? {
             if let Event::Key(key_event) = event::read()? {
                 match key_event.code {
@@ -27,9 +51,18 @@ fn main() -> Result<(), Box<dyn Error>> {
                 }
             }
         }
+
+        // draw & render
+        let _ = render_tx.send(curr_frame);
+        thread::sleep(Duration::from_millis(1));
     }
 
     // cleanup
+    drop(render_tx);
+    render_handle.join().unwrap();
+
+    stdout.execute(Show)?;
+    stdout.execute(LeaveAlternateScreen)?;
     terminal::disable_raw_mode()?;
 
     Ok(())
